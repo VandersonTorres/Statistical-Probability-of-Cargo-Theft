235afff6b28cebdb1f2a17ddb92f6426ec255fd7
Vanderson <vanderson.torres1@hotmail.com>
2023-11-24 13:44:35 -0300

    initial implements

--- additive_regression/main.py
+++ additive_regression/main.py
@@ -1,1 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from prophet import Prophet
 
+data = pd.read_csv("additive_regression/cargo_thefts_dates.csv")
+data["Date"] = pd.to_datetime(data["Date"])
+date_range = pd.date_range(start=data["Date"].min(), end=data["Date"].max(), freq="D")
+
+full_dates = pd.DataFrame({"Date": date_range})
+full_dates["Date"] = pd.to_datetime(full_dates["Date"])
+full_data = pd.merge(full_dates, data, on="Date", how="left").fillna(0)
+full_data.columns = ["ds", "y"]
+
+model = Prophet()
+model.fit(full_data)
+future = model.make_future_dataframe(periods=45)
+forecast = model.predict(future)
+thefts_dates = data[data["Value"] > 0].copy()
+thefts_dates["thefts_amount"] = thefts_dates["Value"]
+
+fig = model.plot(forecast)
+
+for idx, row in thefts_dates.iterrows():
+    plt.plot([row["Date"], row["Date"]], [0, row["thefts_amount"]],
+             color="red", linestyle="--", alpha=0.7, linewidth=0.5)
+
+future_index = forecast[forecast["ds"] > data["Date"].max()].index.tolist()
+
+plt.fill_between(forecast["ds"].iloc[future_index],
+                 forecast["yhat_lower"].iloc[future_index],
+                 forecast["yhat_upper"].iloc[future_index],
+                 color="lightgreen", alpha=0.3, label="Probability std deviation")
+
+plt.plot(forecast["ds"].iloc[future_index], forecast["yhat"].iloc[future_index],
+         color="green", label="Predict", linewidth=1)
+
+for index, row in forecast.iloc[future_index].iterrows():
+    if row["yhat"] > 0.05:
+        plt.plot(row["ds"], 1, marker="o", markersize=2, color="purple")
+        plt.plot([row["ds"], row["ds"]], [0.05, 1], color="purple",
+                 linestyle="-", alpha=0.3, linewidth=0.3)
+        plt.text(row["ds"], 0.5, row["ds"].strftime("%Y-%m-%d"),
+                 rotation=90, ha="center", color="purple")
+
+plt.xlabel("PERIOD", fontsize=12, fontweight="bold", color="black", labelpad=10)
+plt.ylabel("VALUES", fontsize=12, fontweight="bold", color="black", labelpad=10)
+plt.yticks([0, 0.05, 0.10, 1])
+plt.ylim(-0.3, 1.1)
+plt.legend(loc="upper left", bbox_to_anchor=(0, 0.89),
+           shadow=True, ncol=1, facecolor="dimgray")
+
+plt.show()
---
+++ linear_regression/main.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
+class CargoTheftForecast:
+    def __init__(self, file_path):
+        self.data = self.load_data(file_path)
+        self.X = self.data["month"].values.reshape(-1, 1)
+        self.y = self.data["thefts"].values
+        self.model = self.model_training()
+
+    def load_data(self, path):
+        data = pd.read_csv(path)
+        return data
+
+    def model_training(self):
+        model = LinearRegression()
+        model.fit(self.X, self.y)
+        return model
+
+    def to_predict(self, month):
+        month_predict = self.model.predict(month)
+        return round(month_predict[0])
+
+    def to_plot(self, month, month_predict):
+        colors = {
+            "real_data": "royalblue",
+            "linear_regression": "orangered",
+            "prediction": "limegreen",
+            "line_of_prediction": "indigo",
+            "bar": "black"
+        }
+
+        y_rounded = [round(val) for val in self.y]
+
+        plt.figure(figsize=(10, 6))
+        plt.bar(self.X.flatten(), y_rounded, color=colors["bar"], label="Dados de Roubos")
+        plt.scatter(self.X, y_rounded, color=colors["real_data"], label="Dados reais", zorder=2)
+        plt.plot(self.X, self.model.predict(self.X), color=colors["linear_regression"], linewidth=2,
+                 label="Regressão Linear")
+        plt.plot(month, month_predict, marker="o", color=colors["prediction"],
+                 label=f"Previsão para novembro: {month_predict}")
+        plt.plot(np.concatenate((self.X, month)), np.concatenate((y_rounded, [month_predict])),
+                 linestyle="--", color=colors["line_of_prediction"], label="Linha de Previsão")
+        plt.xlabel("Série histórica", fontweight="bold")
+        plt.ylabel("Quantidade de Roubos", fontweight="bold")
+        plt.xticks(
+            [
+                1,   2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12,
+                13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
+            ],
+            [
+                "Jan22", "Fev22", "Mar22", "Abr22", "Mai22", "Jun22", "Jul22", "Ago22", "Set22", "Out22", "Nov22", "Dez22",
+                "Jan23", "Fev23", "Mar23", "Abr23", "Mai23", "Jun23", "Jul23", "Ago23", "Set23", "Out23", "Nov23"
+            ]
+        )
+        legend = plt.legend()
+        legend.get_frame().set_facecolor("lightgray")
+        plt.title("Previsão de Roubos para novembro".upper(), fontweight="bold")
+        plt.grid(True, linestyle="--", alpha=0.3)
+        plt.show()
+
+    def run_app(self, month):
+        month_predict = self.to_predict(month)
+        print(f"Previsão de roubos para o mês de novembro: {month_predict}")
+        self.to_plot(month, month_predict)
+
+
+if __name__ == "__main__":
+    file_path = "linear_regression/statistic_data.csv"
+    regression = CargoTheftForecast(file_path)
+
+    november = np.array([[23]])
+    regression.run_app(november)
